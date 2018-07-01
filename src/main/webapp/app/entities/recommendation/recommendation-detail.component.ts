705f4a52aa9d52c30dbab4170ee5356acb94243d
Javier Hernán Caballero García <caballerojavier13@gmail.com>
2018-07-01 15:56:31 -0300

    Finish iframe bonita

--- src/main/webapp/app/entities/recommendation/recommendation-detail.component.ts
+++ src/main/webapp/app/entities/recommendation/recommendation-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import {Component, OnInit, OnDestroy, ViewChild, AfterViewInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import { Subscription } from 'rxjs/Rx';
 import { JhiEventManager } from 'ng-jhipster';
@@ -8,6 +8,7 @@ import { Recommendation } from './recommendation.model';
 import { RecommendationService } from './recommendation.service';
 import {Observable} from 'rxjs/Observable';
 import {AuditProcessRecommendation} from '../audit-process-recommendation/audit-process-recommendation.model';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
     selector: 'jhi-recommendation-detail',
@@ -16,33 +17,44 @@ import {AuditProcessRecommendation} from '../audit-process-recommendation/audit-
 export class RecommendationDetailComponent implements OnInit, OnDestroy {
 
     recommendation: Recommendation;
+    auditProcessRecom: AuditProcessRecommendation;
     private subscription: Subscription;
     private eventSubscriber: Subscription;
     isSaving: boolean;
     allProcessReviewed: boolean;
     hostname;
+    isShowedModal = false;
+    iframeUrl: SafeResourceUrl;
 
     constructor(
         private eventManager: JhiEventManager,
         private recommendationService: RecommendationService,
         private route: ActivatedRoute,
         private router: Router,
-        private location: Location
+        private location: Location,
+        private sanitizer: DomSanitizer
     ) {
     }
 
     ngOnInit() {
+        this.iframeUrl = this.sanitizer.bypassSecurityTrustResourceUrl('');
         this.hostname = location.hostname;
         this.subscription = this.route.params.subscribe((params) => {
             this.load(params['id']);
         });
         this.registerChangeInRecommendations();
+        this.auditProcessRecom = null;
     }
 
     load(id) {
         this.recommendationService.find(id).subscribe((recommendation) => {
             this.recommendation = recommendation;
             this.checkProcessReview();
+            if (this.auditProcessRecom != null) {
+                const filterList: AuditProcessRecommendation[] = this.recommendation.auditProcessRecommendationSet
+                    .filter((item: AuditProcessRecommendation) => item.id === this.auditProcessRecom.id);
+                this.isShowedModal = !filterList[0].reviewed;
+            }
         });
     }
 
@@ -64,6 +76,22 @@ export class RecommendationDetailComponent implements OnInit, OnDestroy {
             this.allProcessReviewed = true;
         }
 
+    }
+
+    showModal(auditProcessRecom) {
+        this.auditProcessRecom = auditProcessRecom;
+        if (!auditProcessRecom.reviewed) {
+            this.iframeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+                'http://' + this.hostname + ':35407/bonita/loginservice?username=auditor1&password=auditor1&redirectUrl=apps/aresvi/home/?case_id='
+                + auditProcessRecom.bonitaBpmCaseId
+            );
+            this.isShowedModal = true;
+        }
+    }
+
+    hideModal() {
+        this.load(this.recommendation.id);
+        this.isShowedModal = false;
     }
 
     save() {
